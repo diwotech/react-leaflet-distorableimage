97f342315af46991c95adf8fcbf6cd641125f1b5
AlexeyHabarov <alexey.habarov@diwo.tech>
2022-09-15 22:11:37 +0300

    add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { ReactLeafletDistortableImage } from "react-leaflet-distortableimage";
-import L from "leaflet";
+import { MyComponent } from "@diwotech/testlib";
 import { MapContainer, TileLayer } from "react-leaflet";
 import Image from "./example.jpg";
+import "leaflet";
+import L from "leaflet";
+
 const App = () => {
-  // const [onShow, setOnShow] = useState(true);
-  console.log("ReactLeafletDistortableImage", ReactLeafletDistortableImage);
+  const [onShow, setOnShow] = useState(true);
   return (
     <div>
-      {/* <button onClick={() => setOnShow(!onShow)}> Click </button> */}
+      <button onClick={() => setOnShow(!onShow)}> Click </button>
       <MapContainer
         bounds={[
           [43.786293, 15.64765, 0],
@@ -16,10 +17,11 @@ const App = () => {
         ]}
       >
         <TileLayer noWrap={true} attribution="" url="http://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}" />
-        {/* {onShow ? ( */}
-          <ReactLeafletDistortableImage
+
+        {onShow ? (
+          <MyComponent
             url={Image}
-            bounds={[
+            corners={[
               new L.latLng(43.78710550492949, 15.647438805314396),
               new L.latLng(43.78710550492949, 15.655914504316957),
               new L.latLng(43.78098644922989, 15.647438805314396),
@@ -29,11 +31,11 @@ const App = () => {
             actions={[L.RotateAction, L.ScaleAction, L.OpacityAction]}
             selected={true}
             suppressToolbar={false}
+            zIndex={200}
           />
-        {/* ) : null} */}
+        ) : null}
       </MapContainer>
     </div>
   );
 };
-
 export default App;
